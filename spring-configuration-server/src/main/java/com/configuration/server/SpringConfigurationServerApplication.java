f4a5ee3ebacbb1e40264cac8b7c4ee477de34c50
vinod <vinod@Vinod>
2022-09-10 17:31:43 +0530

    asdasdasd

---
+++ spring-configuration-server/src/main/java/com/configuration/server/SpringConfigurationServerApplication.java
@@ -0,0 +1,13 @@
+package com.configuration.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigurationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigurationServerApplication.class, args);
+	}
+
+}
